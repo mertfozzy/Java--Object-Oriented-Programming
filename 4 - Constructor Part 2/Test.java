b4e7b7bbd975439092621eb8fb92e1fa1de4ba47
Mert Altuntaş <mertfozzy@gmail.com>
2021-03-10 20:25:15 +0300

    Add files via upload

---
+++ 4 - Constructor Part 2/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        Account account1 = new Account();
+        Account account2 = new Account("Ahmet Can", "example@gmail.com", "12345678");
+
+        account2.bilgiGoster();
+
+
+    }
+
+
+
+}
